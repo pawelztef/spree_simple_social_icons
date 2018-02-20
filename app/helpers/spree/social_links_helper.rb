fd387835d0ea0f5db9f1f3569bf87a54e22dc3ad
pawelztef <pawelztef@gmail.com>
2018-02-20 11:59:00 +0000

    build links helper added

--- app/helpers/spree/social_links_helper.rb
+++ app/helpers/spree/social_links_helper.rb
@@ -1,9 +1,19 @@
 module Spree::SocialLinksHelper
-    def show_visibility entry 
-      if entry.visible
-       content_tag(:i, '', :class => 'glyphicon glyphicon-check')
-      else
-       content_tag(:i, '', :class => 'glyphicon glyphicon-unchecked')
+  def show_visibility entry 
+    if entry.visible
+      content_tag(:i, '', :class => 'glyphicon glyphicon-check')
+    else
+      content_tag(:i, '', :class => 'glyphicon glyphicon-unchecked')
+    end
+  end
+  def build_links
+    links = Spree::SocialLink.get_links
+    if links.present?
+      content_tag :div, class: 'social-links' do
+        links.get_links.map do |link| 
+          concat(image_tag link.icon_image.url(:mid))
+        end
       end
     end
+  end
 end
